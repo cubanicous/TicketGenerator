08cf5c0d9eed13317aa57a3b95d2b6111a88d0ad
cubanicous <cubanicous@icloud.com>
2021-02-14 08:56:30 -0700

    updated TicketGeneratorApplicationTests.java

--- src/test/java/com/simplilearn/TicketGenerator/TicketGeneratorApplicationTests.java
+++ src/test/java/com/simplilearn/TicketGenerator/TicketGeneratorApplicationTests.java
@@ -2,7 +2,6 @@ package com.simplilearn.TicketGenerator;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
